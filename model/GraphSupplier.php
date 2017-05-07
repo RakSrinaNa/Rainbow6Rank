4d92613e1b5f3d93c1619716271595c6c082741e
Thomas Couchoud <zerderr@gmail.com>
2017-05-07 22:50:12 +0200

    Revert "_"
    This reverts commit 53a8ab6761e2974cc23f3db93dd3ab8d64022748.

--- model/GraphSupplier.php
+++ model/GraphSupplier.php
@@ -65,7 +65,8 @@
 									lineThickness: 2,
 									bulletSize: 8,
 									balloonFunction: function (graphDataItem) {
-										<?php $this->getBalloonFunction() ?>
+										var date = graphDataItem.category;
+										return username + '<br>' + ("0" + date.getDate()).slice(-2) + "-" + ("0" + (date.getMonth() + 1)).slice(-2) + "-" + date.getFullYear() + " " + ("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2) + '<br/><b><span style="font-size:14px;">' + graphDataItem.values.value + '</span></b>';
 									}
 								});
 
@@ -157,8 +158,7 @@
 									zoomable: true,
 									valueZoomable: true,
 									categoryBalloonDateFormat: 'MMMM DD HH:NN',
-									showNextAvailable: true,
-
+									showNextAvailable: true
 								},
 								numberFormatter: {
 									precision: -1,
@@ -217,11 +217,6 @@
 		}
 
 		abstract function getPoint($player);
-
-		function getBalloonFunction()
-		{
-			return 'var date = graphDataItem.category; return username + \'<br>\' + ("0" + date.getDate()).slice(-2) + "-" + ("0" + (date.getMonth() + 1)).slice(-2) + "-" + date.getFullYear() + " " + ("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2) + \'<br/><b><span style="font-size:14px;">\' + graphDataItem.values.value + \'</span></b>\';';
-		}
 
 		abstract function getTitle();
 
