bfd5796d81cb823a5e8d15aef7c1534119d37129
Thomas Couchoud <zerderr@gmail.com>
2017-06-03 17:46:12 +0200

    _

--- graphs/RankedSeason6Graph.php
+++ graphs/RankedSeason6Graph.php
@@ -3,6 +3,8 @@
 
 	class RankedSeason6Graph extends GraphSupplier
 	{
+		private $ranks = array('Gold I' => array('from' => 3100, 'to' => 3300), 'Gold II' => array('from' => 2900, 'to' => 3100), 'Gold III' => array('from' => 2700, 'to' => 2900), 'Gold IV' => array('from' => 2500, 'to' => 2700), 'Silver I' => array('from' => 2400, 'to' => 2500), 'Silver II' => array('from' => 2300, 'to' => 2400), 'Silver III' => array('from' => 2200, 'to' => 2300), 'Silver IV' => array('from' => 2100, 'to' => 2200), 'Bronze I' => array('from' => 2000, 'to' => 2100), 'Bronze II' => array('from' => 1900, 'to' => 2000), 'Bronze III' => array('from' => 1800, 'to' => 1900), 'Bronze IV' => array('from' => 1700, 'to' => 1800), 'Copper I' => array('from' => 1600, 'to' => 1700), 'Copper II' => array('from' => 1500, 'to' => 1600), 'Copper III' => array('from' => 1400, 'to' => 1500));
+
 		function getPoint($player)
 		{
 			if(!isset($player['seasons']) || !isset($player['seasons']['6']))
@@ -11,7 +13,18 @@
 				return null;
 			$point = array('stat' => 0);
 			$point['stat'] = $player['seasons']['6']['emea']['ranking']['rating'];
+			$point['rank'] = $this->getRank($point['stat']);
 			return $point;
+		}
+
+		function getRank($score)
+		{
+			foreach($this->ranks as $index => $value)
+			{
+				if($score >= $value['from'] && $score < $value['to'])
+					return $index;
+			}
+			return 'Unranked';
 		}
 
 		function getTitle()
@@ -26,13 +39,11 @@
 
 		function getGuides()
 		{
-			$ranks = array('Gold I' => array('from' => 3100, 'to' => 3300), 'Gold II' => array('from' => 2900, 'to' => 3100), 'Gold III' => array('from' => 2700, 'to' => 2900), 'Gold IV' => array('from' => 2500, 'to' => 2700), 'Silver I' => array('from' => 2400, 'to' => 2500), 'Silver II' => array('from' => 2300, 'to' => 2400), 'Silver III' => array('from' => 2200, 'to' => 2300), 'Silver IV' => array('from' => 2100, 'to' => 2200), 'Bronze I' => array('from' => 2000, 'to' => 2100), 'Bronze II' => array('from' => 1900, 'to' => 2000), 'Bronze III' => array('from' => 1800, 'to' => 1900), 'Bronze IV' => array('from' => 1700, 'to' => 1800), 'Copper I' => array('from' => 1600, 'to' => 1700), 'Copper II' => array('from' => 1500, 'to' => 1600), 'Copper III' => array('from' => 1400, 'to' => 1500));
-
 			$i = 0;
 			$guideColors = array('#555555', '#aaaaaa');
 
 			$guides = array();
-			foreach($ranks as $rankName => $rankDatas)
+			foreach($this->ranks as $rankName => $rankDatas)
 			{
 				$guides[] = array('fillAlpha' => 0.3, 'lineAlpha' => 1, 'lineThickness' => 1, 'value' => $rankDatas['from'], 'toValue' => $rankDatas['to'], 'valueAxis' => 'pointsAxis', 'label' => $rankName, 'inside' => true, 'position' => 'right', 'fillColor' => $guideColors[$i++ % count($guideColors)]);
 			}
