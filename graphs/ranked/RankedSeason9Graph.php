f4b76cd88cce1c5bc5930df399e591db4636ac3d
Thomas COUCHOUD <zerderr@gmail.com>
2018-04-08 20:37:21 +0200

    Update season ending

--- graphs/ranked/RankedSeason9Graph.php
+++ graphs/ranked/RankedSeason9Graph.php
@@ -4,7 +4,7 @@
 	{
 		require_once __DIR__ . '/RankedSeasonGraph.php';
 
-		class RankedSeason9Graph extends RankedSeasonGraph
+		class RankedSeason9Graph extends RankedSeasonGraph //Chimera
 		{
 			private $endDate = -1;
 
